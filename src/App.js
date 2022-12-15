afcbe19bd33f1ea89434afe2f2877ec23f946fb7
UmeshMehta1 <umeshkumar.mehta@koshistjames.edu.np>
2022-12-15 19:09:27 +0545

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Link, Routes, Route, useNavigate } from 'react-router-dom';
+import Footer from './Footer';
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [dice, setDice] = useState(1);
+  const [error, setError] = useState(null);
+  const [score, setScore] = useState(0);
+  const Number = [1, 2, 3, 4, 5, 6];
+
+  const startGameHandler = () => {
+    setGameStarted(true);
+  };
+
+  const getRandomNo = () => {
+    if (selectedNumber) {
+      const getRandomNo = Math.ceil(Math.random() * 6);
+      setDice(getRandomNo);
+
+      if (selectedNumber === getRandomNo) {
+        setScore((prev) => prev + 5);
+      } else {
+        setScore((prev) => prev - 2);
+      }
+    } else {
+      setError('Please Select A Number');
+    }
+  };
+
+  const onNumberClicked = (value) => {
+    setSelectedNumber(value);
+    setError(null);
+  };
+
+  const onResetHandaler = () => {
+    setScore(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {gameStarted ? (
+        <>
+          <div className='previous'>
+            {/* <button onClick={() => navigate(-1)}>Go Back</button> */}
+          </div>
+          <div className='topContainer'>
+            <header style={{ color: error ? 'red' : 'black' }}>
+              <h1>{error ? error : 'Select Number'}</h1>
+            </header>
+            <section>
+              <div className='topMap'>
+                {Number.map((value) => (
+                  <div
+                    className='inMap'
+                    style={{
+                      backgroundColor:
+                        selectedNumber === value ? 'green' : 'black',
+                    }}
+                    key={value}
+                    onClick={() => onNumberClicked(value)}>
+                    {value}
+                  </div>
+                ))}
+              </div>
+            </section>
+
+            <div className='dice' onClick={getRandomNo}>
+              <img src={`./images/dice/dice${dice}.png`} alt='' />
+            </div>
+
+            <div className='para'>
+              <p>Click on dice to roll</p>
+            </div>
+
+            <div
+              className='score'
+              style={{ color: score >= 0 ? 'green' : 'red' }}>
+              {score}
+            </div>
+            <div className='totalScore'>Total Score</div>
+            <button className='resetBtn' onClick={onResetHandaler}>
+              Reset Score
+            </button>
+          </div>
+          <div className='rules'>
+            <h1>Game Rules:-</h1>
+            <p>1) Select Number any number.</p>
+            <p>2) Click on dice image to roll it.</p>
+            <p>
+              3) Select number is equal to obtained dice result then you will
+              get same point of dice.
+            </p>
+            <p>4) if You are Wrong Score will be Increased by 5 points.</p>
+            <p>4) if You are Wrong Score will be reducted by 2 points.</p>
+          </div>
+          <Footer />
+        </>
+      ) : (
+        <div className='container'>
+          <div className='image'>
+            <img src='./images/dices.png' alt='' />
+          </div>
+
+          <div className='header'>
+            <h1>The Dice Game</h1>
+            <button onClick={startGameHandler}>Start Game</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+export default function Footer() {
+  return (
+    <>
+      <div className='footer'>
+        <h4>Copyright © 2022 Umesh. All Rights Reserved</h4>
+      </div>
+    </>
+  );
+}
